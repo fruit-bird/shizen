bf4361e52c18dd64947a1f76a4256c97ac2f2176
fruit-bird <github.ru3w9@slmail.me>
2023-12-16 20:11:43 +0100

    more audio buffer features

--- shizen/src/buffers/audio/audio_buffer.rs
+++ shizen/src/buffers/audio/audio_buffer.rs
@@ -1,18 +1,106 @@
-#[derive(Debug, Default, PartialEq)]
-pub struct AudioBuffer<T = f32> {
-    pub(crate) samples: Vec<T>,
+use std::vec::IntoIter;
+
+use crate::buffers::audio::ChannelType;
+use crate::components::AudioProcessor;
+
+pub type Sample = f32;
+
+#[derive(Debug, PartialEq)]
+pub struct AudioBuffer {
+    pub samples: Vec<Sample>,
+    pub sample_rate: f32,
+    pub buffer_size: usize,
+    pub channel_type: ChannelType,
 }
 
-impl<T> AudioBuffer<T> {
-    pub fn new(samples: Vec<T>) -> Self {
-        Self { samples }
+impl Default for AudioBuffer {
+    fn default() -> Self {
+        Self {
+            samples: vec![0.0; (2 << 9) * ChannelType::default() as usize],
+            sample_rate: 44100.0,
+            buffer_size: 2 << 9,
+            channel_type: ChannelType::default(),
+        }
+    }
+}
+
+impl AudioBuffer {
+    pub fn new_stereo(samples: Vec<Sample>) -> Self {
+        Self {
+            buffer_size: samples.len() / 2,
+            samples,
+            channel_type: ChannelType::Stereo,
+            ..Default::default()
+        }
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &T> {
+    pub fn new_mono(samples: Vec<Sample>) -> Self {
+        Self {
+            buffer_size: samples.len(),
+            samples,
+            channel_type: ChannelType::Mono,
+            ..Default::default()
+        }
+    }
+
+    // TODO: do something better than wrap everything in an Option
+    //       maybe return a tuple with left full and right empty if mono
+    //       or return the same signal twice? or split in half, like half the gain??
+    //       it's cheap either way cause it's a lazy iterator
+    /// Returns None if the AudioBuffer is mono
+    pub fn split_channels(
+        &self,
+    ) -> Option<(impl Iterator<Item = &Sample>, impl Iterator<Item = &Sample>)> {
+        let (l, r) = match self.is_stereo() {
+            true => self.samples.split_at(self.buffer_size),
+            false => return None,
+        };
+
+        Some((l.iter(), r.iter()))
+    }
+
+    pub fn process_with(self, component: &mut dyn AudioProcessor) -> Self {
+        component.process_audio(self)
+    }
+
+    #[must_use]
+    pub const fn is_stereo(&self) -> bool {
+        matches!(self.channel_type, ChannelType::Stereo)
+    }
+
+    #[must_use]
+    pub const fn is_mono(&self) -> bool {
+        matches!(self.channel_type, ChannelType::Mono)
+    }
+
+    #[inline]
+    pub fn iter(&self) -> impl Iterator<Item = &Sample> {
         self.samples.iter()
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+    #[inline]
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Sample> {
         self.samples.iter_mut()
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.samples.len()
+    }
+
+    #[must_use]
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.samples.is_empty()
+    }
+}
+
+impl IntoIterator for AudioBuffer {
+    type Item = Sample;
+    type IntoIter = IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.samples.into_iter()
     }
 }
--- shizen/src/buffers/audio/mod.rs
+++ shizen/src/buffers/audio/mod.rs
@@ -1,9 +1,11 @@
 mod audio_buffer;
+mod channel_type;
 
-pub use audio_buffer::AudioBuffer;
+pub use audio_buffer::{AudioBuffer, Sample};
+pub use channel_type::ChannelType;
 
 #[test]
-fn process_audio() {
-    let mut audio = AudioBuffer::new(vec![0, 1, 3, 5]);
-    let processed = audio.iter_mut().map(|s| *s * 2);
+fn audio_processing_concept() {
+    let mut audio = AudioBuffer::new_stereo(vec![0.0, 1.0, 3.0, 5.0]);
+    let _processed = audio.iter_mut().map(|s| *s * 2.0);
 }
