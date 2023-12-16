47c153e8ed74883c048b436ba8f5d70dc8252676
fruit-bird <github.ru3w9@slmail.me>
2023-12-16 20:22:38 +0100

    grouped audio tests in a tmod

--- shizen/src/buffers/audio/mod.rs
+++ shizen/src/buffers/audio/mod.rs
@@ -4,8 +4,13 @@ mod channel_type;
 pub use audio_buffer::{AudioBuffer, Sample};
 pub use channel_type::ChannelType;
 
-#[test]
-fn audio_processing_concept() {
-    let mut audio = AudioBuffer::new_stereo(vec![0.0, 1.0, 3.0, 5.0]);
-    let _processed = audio.iter_mut().map(|s| *s * 2.0);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn audio_processing_concept() {
+        let mut audio = AudioBuffer::new_stereo(vec![0.0, 1.0, 3.0, 5.0]);
+        let _processed = audio.iter_mut().map(|s| *s * 2.0);
+    }
 }
