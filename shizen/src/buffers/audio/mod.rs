75530e03ad7823f5a4a27f24d55bfd6aed2d39e3
fruit-bird <github.ru3w9@slmail.me>
2023-12-16 21:22:52 +0100

    Added Audio macro for shorthand return in Components

--- shizen/src/buffers/audio/mod.rs
+++ shizen/src/buffers/audio/mod.rs
@@ -4,6 +4,47 @@ mod channel_type;
 pub use audio_buffer::{AudioBuffer, Sample};
 pub use channel_type::ChannelType;
 
+/// A macro to replace the redundant syntax of:
+/// ```
+/// # let audio_buffer = AudioBuffer::default();
+/// # let output = AudioBuffer::default();
+/// match audio_buffer.channel_type {
+///     ChannelType::Mono => AudioBuffer::new_mono(output),
+///     ChannelType::Stereo => AudioBuffer::new_stereo(output),
+/// }
+/// ```
+/// with the shorter
+/// ```
+/// Audio![output as audio_buffer.channel_type]
+/// ```
+///
+/// This macro behaves similarly to a parameterized copy constructor for [`AudioBuffer`]
+///
+/// # Usage
+/// ```
+/// Audio![output as Mono];
+/// Audio![output as Stereo];
+/// Audio![output as audio_buffer.channel_type];
+/// ```
+#[macro_export]
+macro_rules! Audio {
+    ($output:ident as Mono) => {
+        AudioBuffer::new_mono($output)
+    };
+
+    ($output:ident as Stereo) => {
+        AudioBuffer::new_stereo($output)
+    };
+
+    ($output:ident as $channel_type:expr) => {
+        match $channel_type {
+            crate::buffers::ChannelType::Mono => AudioBuffer::new_mono($output),
+            crate::buffers::ChannelType::Stereo => AudioBuffer::new_stereo($output),
+            // _ => unimplemented!("Whoops, this channel type has yet to be implemented"),
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
