9fa3a9f63cf2192ff047bf9dd078b9846db26668
fruit-bird <github.ru3w9@slmail.me>
2023-12-31 14:23:47 +0100

    populated error enum

--- shizen/src/errors/mod.rs
+++ shizen/src/errors/mod.rs
@@ -3,16 +3,21 @@ use std::{error::Error, fmt::Display};
 // An opaque error type for the entire crate
 // maybe use thiserror when the enum is populated
 #[derive(Debug)]
-pub enum PluginError {}
+pub enum PluginError {
+    Initialize,
+    Terminate,
+}
 
 impl Display for PluginError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            _ => writeln!(f,),
+            Self::Initialize => writeln!(f, "Failed to initialize plugin"),
+            Self::Terminate => writeln!(f, "Failed to terminate plugin"),
+            // _ => writeln!(f,),
         }
     }
 }
 
 impl Error for PluginError {}
 
-pub type PluginResult<T> = std::result::Result<T, PluginError>;
+pub type PluginResult<T = ()> = std::result::Result<T, PluginError>;
