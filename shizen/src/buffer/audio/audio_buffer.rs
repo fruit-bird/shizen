862b3e603a4d278700d3041837eaf4f2478834e5
fruit-bird <github.ru3w9@slmail.me>
2024-02-25 14:28:06 +0100

    another backup, still doesn't work

--- shizen/src/buffer/audio/audio_buffer.rs
+++ shizen/src/buffer/audio/audio_buffer.rs
@@ -3,25 +3,36 @@ use std::marker::PhantomData;
 use super::AudioIterator;
 
 pub type Sample = f32;
-pub type MonoBuffer<'a, const N: usize> = AudioBuffer<'a, N, 1>;
-pub type StereoBuffer<'a, const N: usize> = AudioBuffer<'a, N, 2>;
+pub type MonoBuffer<'a> = AudioBuffer<'a, 1>;
+pub type StereoBuffer<'a> = AudioBuffer<'a, 2>;
 
 #[derive(Debug, PartialEq)]
-pub struct AudioBuffer<'a, const SAMPLES: usize, const CHANNELS: usize> {
-    pub samples: [[Sample; SAMPLES]; CHANNELS],
-    marker: PhantomData<&'a ()>,
+pub struct AudioBuffer<'a, const CHANNELS: usize> {
+    pub(crate) samples: *const [*mut [Sample]; CHANNELS],
+    marker: PhantomData<&'a [Sample]>,
 }
 
-impl<'a, const SAMPLES: usize, const CHANNELS: usize> AudioBuffer<'a, SAMPLES, CHANNELS> {
-    pub const fn new(samples: [[Sample; SAMPLES]; CHANNELS]) -> Self {
+impl<'a, const CHANNELS: usize> AudioBuffer<'a, CHANNELS> {
+    pub const fn new(samples: [&'a [Sample]; CHANNELS]) -> Self {
         Self {
-            samples,
+            samples: samples.as_ptr() as *const _,
             marker: PhantomData,
         }
     }
 
     #[inline]
-    pub fn iter(&self) -> AudioIterator<'a, SAMPLES, CHANNELS> {
+    pub fn iter(&self) -> AudioIterator<'a, CHANNELS> {
         AudioIterator::new(*self)
+    }
+
+    #[inline]
+    pub const fn len(&self) -> usize {
+        unsafe {
+            self.samples
+                .as_ref()
+                .and_then(|s| s.get(0))
+                .and_then(|s| s.as_ref())
+                .map_or(0, |s| s.len())
+        }
     }
 }
