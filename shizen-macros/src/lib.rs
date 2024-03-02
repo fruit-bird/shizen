d66e2c0f61a9e7349a6b9f816ab49e661b75e4bb
fruit-bird <github.ru3w9@slmail.me>
2024-03-02 22:47:51 +0100

    renamed macros crate + revamped entire macro use

--- shizen-macros/src/lib.rs
+++ shizen-macros/src/lib.rs
@@ -1,12 +1,15 @@
+mod ast;
 mod attr;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, Error, ItemFn};
 
 #[proc_macro_attribute]
-pub fn shizen(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn shizen(args: TokenStream, input: TokenStream) -> TokenStream {
+    let parsed_args = parse_macro_input!(args as ast::PluginArgs);
     let parsed_input = parse_macro_input!(input as ItemFn);
-    attr::plugin_impl_alt(parsed_input)
+
+    attr::plugin_impl(parsed_args, parsed_input)
         .unwrap_or_else(Error::into_compile_error)
         .into()
 }
