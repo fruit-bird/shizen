e3a637c4c463cf37b53cd27b3b24b579fd2bb9d7
fruit-bird <github.ru3w9@slmail.me>
2024-07-21 17:22:21 +0100

    updated components to follow the new API

--- components/src/delay.rs
+++ components/src/delay.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use shizen::prelude::*;
 
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
@@ -5,40 +7,25 @@ pub struct DelayComponent {
     pub delay_time: f32,
     pub feedback: f32,
     pub wet_dry: f32,
-    pub buffer: Vec<f32>,
 }
 
 impl DelayComponent {
-    pub fn new(delay_time: f32, feedback: f32, wet_dry: f32) -> Self {
+    pub const fn new(delay_time: f32, feedback: f32, wet_dry: f32) -> Self {
         Self {
             delay_time,
             feedback,
             wet_dry,
-            buffer: Vec::new(),
         }
     }
 }
 
-impl AudioProcessor for DelayComponent {
-    fn process_audio(&mut self, audio_buffer: AudioIterator) -> AudioBuffer {
-        let output = audio_buffer
-            .enumerate()
-            .map(|(i, sample)| {
-                let delayed_sample = self.buffer.get(i).unwrap_or(&0.0);
-                let wet_signal = sample + delayed_sample * self.feedback;
-                let dry_signal = sample;
-
-                let mixed_sample = wet_signal * self.wet_dry + dry_signal * (1.0 - self.wet_dry);
-                self.buffer.push(*sample);
-
-                if self.buffer.len() > self.delay_time as usize {
-                    self.buffer.remove(0); // FIFO
-                }
-
-                mixed_sample
-            })
-            .collect();
-
-        Audio![output as Stereo]
+impl<const CH: usize> AudioProcessor<CH> for DelayComponent {
+    fn process_samples(&self, samples: [Sample; CH]) -> [Sample; CH] {
+        let mut delay_buffer = VecDeque::new();
+        samples.map(|s| {
+            delay_buffer.push_back(s);
+            let delayed_sample = delay_buffer.pop_front().unwrap();
+            delayed_sample * self.wet_dry + s * (1.0 - self.wet_dry)
+        })
     }
 }
