b2024be27aa9dfc732c9d39bc3402c5f1c3ca758
fruit-bird <github.ru3w9@slmail.me>
2024-07-28 21:38:36 +0100

    updated lib + version bump

--- shizen/src/lib.rs
+++ shizen/src/lib.rs
@@ -4,14 +4,6 @@ pub mod errors;
 pub mod prelude {
     use super::*;
 
-    pub use crate::MidiProcessor;
     pub use buffer::*;
     pub use errors::*;
-}
-
-use crate::buffer::MidiBuffer;
-
-pub trait MidiProcessor {
-    fn process_midi(&mut self, midi_messages: MidiBuffer)
-        -> <MidiBuffer as IntoIterator>::IntoIter;
 }
